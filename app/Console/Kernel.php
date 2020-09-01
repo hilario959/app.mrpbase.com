8540d9762016d4ca393fb084a2eb737a4e542422
ImperaIT <imperaitnet@gmail.com>
2020-09-01 22:25:20 +0300

    updated kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('orders:check_produced')->hourly();
+         $schedule->command('productions:process')->hourly();
     }
 
     /**
