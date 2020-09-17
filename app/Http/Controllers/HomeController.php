d2352bf0f22b6d898765ce9d567810366bc830f6
ImperaIT <imperaitnet@gmail.com>
2020-09-17 22:02:44 +0300

    add dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Material;
+use App\Order;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,6 +15,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $ordersReceived = Order::where('status', Order::STATUS_RECEIVED)->count();
+        $ordersInProgress = Order::where('status', Order::STATUS_IN_PROGRESS)->count();
+        $ordersDone = Order::where('status', Order::STATUS_DONE)->count();
+        $materials = Material::orderBy('amount', 'asc')->get();
+
+        return view('home', compact('ordersReceived', 'ordersInProgress', 'ordersDone', 'materials'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,73 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+        <div class="col-4">
+            <div class="card mb-4">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <h4 class="card-title font-weight-bold">{{ __('Orders received') }} </h4>
+                    <p class="card-text text-primary">
+                        <i class="fas fa-angle-double-down fa-2x"></i>
+                        <span class="ml-2" style="font-size: 30px;">{{ $ordersReceived }}</span>
+                    </p>
                 </div>
             </div>
         </div>
+        <div class="col-4">
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h4 class="card-title font-weight-bold">{{ __('Orders in progress') }} </h4>
+                    <p class="card-text text-warning">
+                        <i class="fas fa-spinner fa-2x"></i>
+                        <span class="ml-2" style="font-size: 30px;">{{ $ordersInProgress }}</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h4 class="card-title font-weight-bold">{{ __('Orders done') }} </h4>
+                    <p class="card-text text-success">
+                        <i class="fas fa-check fa-2x"></i>
+                        <span class="ml-2" style="font-size: 30px;">{{ $ordersDone }}</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-12 mt-2">
+            <h3 class="font-weight-bold">{{ __('Materials left') }}</h3>
+            <table id="tabulator" class="table table-light table-striped border rounded">
+                <thead>
+                <tr>
+                    <th>{{ __('Name') }}</th>
+                    <th tabulator-formatter="html">{{ __('Amount') }}</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach ($materials as $material)
+                        <tr>
+                            <td>{{ $material->name }}</td>
+                            <td>
+                                <span class="
+                                    font-weight-bold
+                                    @if ($material->amount < 0)
+                                        text-danger
+                                    @elseif ($material->amount > 0)
+                                        text-success
+                                    @else
+                                        text-warning
+                                    @endif
+                                ">
+                                    {{ $material->amount }}
+                                </span>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+
+            </table>
     </div>
 </div>
 @endsection
